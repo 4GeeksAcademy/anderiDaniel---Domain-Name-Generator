5d7d90cede8a89dd51b6f5da94e3c40406e18650
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-02-15 03:49:48 +0000

    cambie nombres de las variables para hacer mas comprensible el codigo

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const domainNameGenerator = () => {
-    let pronoun = ["the", "our"];
-    let adj = ["great", "big"];
-    let noun = ["jogger", "racoon"];
+    let pronouns = ["the", "our"];
+    let adjectives = ["great", "big"];
+    let nouns = ["jogger", "racoon"];
     let domains = [".com", ".net", ".us", ".io", ".ar", ".ve"];
-    for(x in domains){
-        console.log(`the "${domains[x]}" domains:`)
-        for (i in pronoun) {
-        for (j in adj) {
-            for (k in noun) {
-                console.log("  ==>    "+ pronoun[i] + adj[j] + noun[k] + domains[x]);
+    for(dom in domains){
+        console.log(`the "${domains[dom]}" domains:`)
+        for (pro in pronouns) {
+        for (adj in adjectives) {
+            for (noun in nouns) {
+                console.log("  ==>    "+ pronouns[pro] + adjectives[adj] + nouns[noun] + domains[dom]);
             }
         }
     }

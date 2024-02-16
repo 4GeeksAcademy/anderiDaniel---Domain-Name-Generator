5b4b3cbbad3b3cd41fe433fd469d9f6b68017683
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-02-16 00:25:21 +0000

    cambio en la forma de concatenar el domain name a traves del console.log

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const domainNameGenerator = () => {
-    let pronouns = ["the", "our"];
-    let adjectives = ["great", "big"];
-    let nouns = ["jogger", "racoon"];
-    let domains = [".com", ".net", ".us", ".io", ".ar", ".ve"];
-    for(dom in domains){
-        console.log(`the "${domains[dom]}" domains:`)
-        for (pro in pronouns) {
-        for (adj in adjectives) {
-            for (noun in nouns) {
-                console.log("  ==>    "+ pronouns[pro] + adjectives[adj] + nouns[noun] + domains[dom]);
-            }
+  let pronouns = ["the", "our"];
+  let adjectives = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let domains = [".com", ".net", ".us", ".io", ".ar", ".ve"];
+
+  for (dom of domains) {
+    console.log(`the "${dom}" domains:`);
+    for (pro of pronouns) {
+      for (adj of adjectives) {
+        for (noun of nouns) {
+            console.log(`  ==>    ${pro}${adj}${noun}${dom}`)
         }
+      }
     }
   }
 };
-  domainNameGenerator();
+domainNameGenerator();

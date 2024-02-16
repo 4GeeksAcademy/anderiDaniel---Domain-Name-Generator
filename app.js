6beefcabd4365386a748cbe598a67c2ad18a5aad
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-02-16 22:09:06 +0000

    se agrego 'return' a domainNameGenerator para que retorne un array. luego el array se itera para que aparezca en el console.log como en el ejercicio

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ const domainNameGenerator = () => {
   let adjectives = ["great", "big"];
   let nouns = ["jogger", "racoon"];
   let domains = [".com", ".net", ".us", ".io", ".ar", ".ve"];
+  let listOfDomains = []
 
   for (dom of domains) {
-    console.log(`the "${dom}" domains:`);
+      listOfDomains.push(`the "${dom}" domains:`);
     for (pro of pronouns) {
       for (adj of adjectives) {
         for (noun of nouns) {
-            console.log(`  ==>    ${pro}${adj}${noun}${dom}`)
+            listOfDomains.push(`  ==>    ${pro}${adj}${noun}${dom}`);
         }
       }
     }
   }
+  return listOfDomains
 };
-domainNameGenerator();
+
+for (i of domainNameGenerator()){
+  console.log(i);
+}

c283a07706f58db58fa4b2a8645807e356f38b9f
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-02-17 01:36:35 +0000

    agregue varios ; que faltaban

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const domainNameGenerator = () => {
   let adjectives = ["great", "big"];
   let nouns = ["jogger", "racoon"];
   let domains = [".com", ".net", ".us", ".io", ".ar", ".ve"];
-  let listOfDomains = []
+  let listOfDomains = [];
 
   for (dom of domains) {
       listOfDomains.push(`the "${dom}" domains:`);
@@ -11,11 +11,11 @@ const domainNameGenerator = () => {
       for (adj of adjectives) {
         for (noun of nouns) {
             listOfDomains.push(`  ==>    ${pro}${adj}${noun}${dom}`);
-        }
-      }
-    }
-  }
-  return listOfDomains
+        };
+      };
+    };
+  };
+  return listOfDomains;
 };
 
 for (i of domainNameGenerator()){

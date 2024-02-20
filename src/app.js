35cc648a9fe8a6d07c72d089e528d92a6d39e61a
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-02-20 00:39:11 +0000

    declare las variables con let y agregue el codigo al src app.js correspondiente. Falta chequear que corra apropiadamente.

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  const domainNameGenerator = () => {
+    let pronouns = ["the", "our"];
+    let adjectives = ["great", "big"];
+    let nouns = ["jogger", "racoon"];
+    let domains = [".com", ".net", ".us", ".io", ".ar", ".ve"];
+    let listOfDomains = [];
+  
+    for (let dom of domains) {
+        // listOfDomains.push(`the "${dom}" domains:`);
+      for (let pro of pronouns) {
+        for (let adj of adjectives) {
+          for (let noun of nouns) {
+              listOfDomains.push(`  ==>    ${pro}${adj}${noun}${dom}`);
+          };
+        };
+      };
+      };
+    return listOfDomains;
+  };
+  
+  for (i of domainNameGenerator()){
+    console.log(i);
+  }
   console.log("Hello Rigo from the console!");
 };

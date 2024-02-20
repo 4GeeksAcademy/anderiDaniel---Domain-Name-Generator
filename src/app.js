aa2b2f300605b82780fef65fa2bec52f57363816
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-02-20 02:39:55 +0000

    agrege el codigo dentro de la funcion window.onload para que funcionee con el archivo html. agregue tambien unos console.log

--- src/app.js
+++ src/app.js
@@ -1,34 +1,32 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  const domainNameGenerator = () => {
-    let pronouns = ["the", "our"];
-    let adjectives = ["great", "big"];
-    let nouns = ["jogger", "racoon"];
-    let domains = [".com", ".net", ".us", ".io", ".ar", ".ve"];
-    let listOfDomains = [];
-  
-    for (let dom of domains) {
-        // listOfDomains.push(`the "${dom}" domains:`);
-      for (let pro of pronouns) {
-        for (let adj of adjectives) {
-          for (let noun of nouns) {
-              listOfDomains.push(`  ==>    ${pro}${adj}${noun}${dom}`);
-          };
-        };
-      };
-      };
-    return listOfDomains;
-  };
-  
-  for (i of domainNameGenerator()){
-    console.log(i);
+  let pronouns = ["the", "our"];
+  let adjectives = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let domains = [".com", ".net", ".us", ".io", ".ar", ".ve"];
+  let listOfDomains = [];
+
+  for (let dom of domains) {
+    // listOfDomains.push(`the "${dom}" domains:`);
+    console.log(`the "${dom}" domains:`);
+    for (let pro of pronouns) {
+      for (let adj of adjectives) {
+        for (let noun of nouns) {
+          // listOfDomains.push(`  ==>    ${pro}${adj}${noun}${dom}`);
+          console.log(`  ==>    ${pro}${adj}${noun}${dom}`);
+        }
+      }
+    }
   }
+  // for (let i of listOfDomains) {
+  //   console.log(i);
+  // }
   console.log("Hello Rigo from the console!");
 };

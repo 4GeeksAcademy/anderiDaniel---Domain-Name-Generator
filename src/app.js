5ff0d74ed9f9e327b7f6549676e07c3ac636fa06
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-03-16 00:26:34 +0000

    Módulo Buenas Prácticas. Se eliminaron los comentarios del app.js y se elimino lo que no se usaba del index.html

--- src/app.js
+++ src/app.js
@@ -1,32 +1,20 @@
-/* eslint-disable */
-// import "bootstrap";
-// import "./style.css";
-
-// import "./assets/img/rigo-baby.jpg";
-// import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
   let pronouns = ["the", "our"];
   let adjectives = ["great", "big"];
   let nouns = ["jogger", "racoon"];
   let domains = [".com", ".net", ".us", ".io", ".ar", ".ve"];
-  let listOfDomains = [];
+
+  // El uso del console.log es parte del ejercicio
 
   for (let dom of domains) {
-    // listOfDomains.push(`the "${dom}" domains:`);
     console.log(`the "${dom}" domains:`);
     for (let pro of pronouns) {
       for (let adj of adjectives) {
         for (let noun of nouns) {
-          // listOfDomains.push(`  ==>    ${pro}${adj}${noun}${dom}`);
           console.log(`  ==>    ${pro}${adj}${noun}${dom}`);
         }
       }
     }
   }
-  // for (let i of listOfDomains) {
-  //   console.log(i);
-  // }
   console.log("Hello Rigo from the console!");
 };
